687b10fd9de5466ece438bd16ec9eaf2ab961b63
nolequen <nolequen@gmail.com>
2019-04-17 10:37:02 +0300

    fix version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "org.jenkins-ci.plugins"
-version = "1.1.0-SNAPSHOT"
+version = "1.1.0"
 description = "The plugin allows to send Jenkins notifications to ICQ"
 
 jenkinsPlugin {
